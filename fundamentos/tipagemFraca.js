5baeb54facbc3daf33b399efbf373fe73bc2adb5
Daniel Oliveira <dnielvitoroliveira456@gmail.com>
2021-11-16 19:30:51 -0300

    Aprendendo tipagem fraca

--- fundamentos/tipagemFraca.js
+++ fundamentos/tipagemFraca.js
@@ -1,12 +1,12 @@
-let qualquer = 'Legal'
-console.log(qualquer)
-console.log(typeof qualquer)
+let qualquer = 'Legal';
+console.log(qualquer);
+console.log(typeof qualquer);
 
-qualquer = 3.1516
-console.log(qualquer)
-console.log(typeof qualquer)
+qualquer = 3.1516;
+console.log(qualquer);
+console.log(typeof qualquer);
 
 //Evitar escolher nomes genericos e siglas e usar comentários. Opte sempre por um codigo limpo e claro
-let valor =''
-let numero = 1
-let pqp = false // Produto Quimico Perigoso... kkk
+let valor = '';
+let numero = 1;
+let pqp = false; // Produto Quimico Perigoso... kkk
