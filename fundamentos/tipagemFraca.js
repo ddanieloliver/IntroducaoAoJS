c308c6f9f65bad3d0fdbf1429b453a042a0be24c
Daniel Oliveira <dnielvitoroliveira456@gmail.com>
2021-11-16 16:25:52 -0300

    Aprendendo number

--- fundamentos/tipagemFraca.js
+++ fundamentos/tipagemFraca.js
@@ -5,3 +5,8 @@ console.log(typeof qualquer)
 qualquer = 3.1516
 console.log(qualquer)
 console.log(typeof qualquer)
+
+//Evitar escolher nomes genericos e siglas e usar comentários. Opte sempre por um codigo limpo e claro
+let valor =''
+let numero = 1
+let pqp = false // Produto Quimico Perigoso... kkk
