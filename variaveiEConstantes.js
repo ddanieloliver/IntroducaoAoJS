3ddd0fd7efb1f872920300e6b4a7986c9a7b2edb
Daniel Oliveira <dnielvitoroliveira456@gmail.com>
2021-11-16 15:49:16 -0300

    aprendendo sobre variaveis e constantes

---
+++ variaveiEConstantes.js
@@ -0,0 +1,12 @@
+var a = 3; //EVITAR USAR VAR... o var possibilita que eu redesigne a variável dentro do mesmo escopo.
+let b = 4; //usar let com mais frequência, pois é mais moderno.
+// |-> são usados para criar uma variável
+var a = 30;
+b = 40;
+
+console.log(a, b);
+
+const c = 5
+// c = 50
+console.log(c)
+//Sempre será melhor eu utilizar constantes. Evitar reatribuir os valores das coisas.
