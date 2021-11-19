21d926a7443445a7fc7e09c24e69258f496f3d5d
Daniel Oliveira <dnielvitoroliveira456@gmail.com>
2021-11-19 11:04:11 -0300

    Praticando var e tomando cuidado aonde posso usar

---
+++ fundamentos/usandoVar.js
@@ -0,0 +1,21 @@
+/*a variável var, diferente do let, torna-se visivel para todo mundo mesmo estando fora do bloco. Exceto no caso da função,
+ pois o escopo da function so é visível dentro dela.*/
+{
+    { 
+        {
+             { var sera = 'Será???' 
+                console.log(sera)
+            }
+         } 
+     } 
+}
+console.log(sera)
+//Escopo é o local aonde a variável é visivel. Até onde ela é acessivel;
+
+function teste () {
+    var local = 123
+    console.log(local)
+}
+
+teste ()
+//console.log(local) -- Não consegue porque var local só é visto dentro da function
