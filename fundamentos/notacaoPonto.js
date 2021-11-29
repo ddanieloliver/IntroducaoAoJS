3b2442b299c5709889c241b9455dfa1713c2584e
Daniel Oliveira <dnielvitoroliveira456@gmail.com>
2021-11-29 09:53:54 -0300

    Entendendo a organização dentro do js

--- fundamentos/notacaoPonto.js
+++ fundamentos/notacaoPonto.js
@@ -1,3 +1,21 @@
-console.log(Math.ceil(6.1))
+console.log(Math.ceil(6.1)) //math aponta um arredondamento e pode se ceil e floor
 
-const any = {}
+const obj1 = {}
+obj1.nome = 'Bola'
+//isso é igual a --> obj1['nome'] = 'Bola
+console.log(obj1.nome)
+
+function Obj(nome) {
+    this.nome = nome    //this.nome significa o nome associado ao objeto que foi criado a partir da função
+                        //this.nome também significa que se pode instanciar o objeto dentro da funcao e esse nome ficará visivel fora do escopo da função
+                        //this significa "essa", ou seja , "essa função" esse objeto dentro dessa função
+    this.exec = function() {
+        console.log('Exec...')
+    }
+}
+
+const obj2 = new Obj('Cadeira')
+const obj3 = new Obj('Mesa')
+console.log(obj2.nome)
+console.log(obj3.nome)
+obj3.exec()
