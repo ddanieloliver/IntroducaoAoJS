5c04c6ddccf23de5b76709a143873d17fcc7543f
Daniel Oliveira <dnielvitoroliveira456@gmail.com>
2021-11-29 10:10:36 -0300

    Exemplos de operadores e como usá-los

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7
+let b = 3 
+
+b += a  //isso significa que estou adicionando "a" ao valor de "b" ou seja, b = b + a
+console.log (b)
+
+b -=  4 // b = b - 4
+console.log(b)
+
+b *= 2 // b = b * 2
+console.log(b)
+
+b /= 2 // b = b / 2
+console.log(b)
+b %= 2 // b = b % 2  //se o valor for par, será imprimido 0, se for impar, 1.
+console.log(b)
+
