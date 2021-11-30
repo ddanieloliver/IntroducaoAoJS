b6c82ba64cdb48348e2bb4ab27b6824749c82e66
Daniel Oliveira <dnielvitoroliveira456@gmail.com>
2021-11-30 15:46:42 -0300

    Alguns exemplos e alertas para usar destructuring

--- fundamentos/destructuring.js
+++ fundamentos/destructuring.js
@@ -20,4 +20,4 @@ console.log(sobrenome, bemHumorada)
 const {endereco: {logradouro, numero, cep }} = pessoa
 console.log(logradouro, numero, cep)
 // cuidado para não tentar acessar um recuso que não existe por exemplo:
-// const{conta{ag: nu:}} isso vai dar erro
+// const{conta{ag: n:}} isso vai dar erro
