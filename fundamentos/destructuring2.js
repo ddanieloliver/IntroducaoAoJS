313fe49a1693e3e2e5837f86d33af75c6e5178f9
Daniel Oliveira <dnielvitoroliveira456@gmail.com>
2021-12-02 20:11:56 -0300

    Alguns exemplos e alertas para usar destructuring a partir de um array

--- fundamentos/destructuring2.js
+++ fundamentos/destructuring2.js
@@ -5,7 +5,7 @@ const [a] = [10]
 console.log(a)
 
 const [n1, , n3, , n5, n6 = 0] = [10, 7, 9, 8];
-console.log (n1, n3, n5, n6)
+console.log (n1, n3, n5, n6);
 
 const [, [, nota]] = [[, 8, 8], [9, 6, 8]] // nota refere-se ao indice 2.
 console.log(nota) // isso significa que tirei o nota = 6 de dentro do array;
