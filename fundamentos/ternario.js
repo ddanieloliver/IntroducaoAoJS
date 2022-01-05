61e23afc1ad50ac43c2388d68579aa378aaeb216
Daniel OLiveira <dnielvitoroliveira456@gmail.com>
2022-01-05 18:41:44 -0300

    Vendo a construção do operador ternario

--- fundamentos/ternario.js
+++ fundamentos/ternario.js
@@ -1,4 +1,8 @@
 const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+// o operador ternario funciona com três operandos;
+// a primeira parte é o nota >= 7
+//segunda parte é se retornar true aparece aprovado
+// a terceira é se retornar false aoarece Reprovado
 
 console.log(resultado(7.1))
 console.log(resultado(6.5))
